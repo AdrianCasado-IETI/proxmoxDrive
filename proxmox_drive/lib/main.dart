e853e5e9f9c3ff3acb406ac622846962db8afcaa
MarcArques <marc.arques.marimon@gmail.com>
2025-02-17 17:10:53 +0100

    file manager commit

--- proxmox_drive/lib/main.dart
+++ proxmox_drive/lib/main.dart
@@ -3,30 +3,5 @@ import 'services/ssh_services.dart';
 import 'services/server_manager.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  print("🚀 Iniciando conexión SSH...");
-
-  SSHService ssh = await SSHService.create(
-    serverName: "root",
-    serverAddress: "192.168.1.100",
-    port: 22,
-    privateKeyPath: "/ruta/a/id_rsa",
-  );
-
-  print("✅ Conexión establecida. Ejecutando comando...");
-
-  String output = await ssh.executeCommand("ls -l /var/lib/lxc/");
-  print("📂 Archivos en el servidor:\n$output");
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const Login()
-    );
-  }
+  
 }
